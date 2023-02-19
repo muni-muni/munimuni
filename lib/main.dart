f502d801aed45654b2bbc839327265b66b11df52
KyahWill <knownbutnotloved@gmail.com>
2023-02-20 02:44:21 +0800

    removed task.dart, improved to do list, will be adding some firm designs sooner or later

--- lib/main.dart
+++ lib/main.dart
@@ -13,11 +13,42 @@ class MuniMuni extends StatefulWidget {
 }
 
 class _MuniMuniState extends State<MuniMuni> {
-  List<String> text = ["test1", "test2", "test3"];
+  List<String> _text = ["test1", "test2", "test3"];
   
+  void _addText(){
+    setState(() {
+      _text.add(_inputTaskController.text);
+    });
+  }
+
+  final _inputTaskController = TextEditingController();
+  @override
+
   Widget getTextWidgets(List<String> strings)
   {
-    return Column(children: strings.map((item) => Text(item)).toList());
+    return Column(children: strings.map((item) => 
+      Container(
+        height: 50,
+        width: 350,
+        decoration: BoxDecoration(
+          color: Colors.green[500],
+          border: Border.all(width:1, color: Colors.grey),
+          borderRadius: BorderRadius.circular(12)    
+        ),
+        child: Align(
+          alignment: Alignment.centerLeft,
+          child:
+          Padding(
+            padding:EdgeInsets.all(8.0),
+            child: Text(item,
+            style: TextStyle(
+              fontSize: 20,
+              color:Colors.grey[900],              
+            )
+          )
+        )
+      )
+    )).toList());
   }  
 
   @override
@@ -29,22 +60,22 @@ class _MuniMuniState extends State<MuniMuni> {
             appBar: AppBar(
               title: const Text('Home')
             ),
-            body: Flex(
-              direction: Axis.vertical,
+            body: ListView(
               children:[
                 Row(
                   mainAxisSize: MainAxisSize.max,
                   children: [
-                    const SizedBox(
-                      width:100,
-                      child: TextField(maxLength: 20,)
+                    Expanded(
+                      child: TextField(
+                        controller:  _inputTaskController,
+                      )
                     ),
-                    FloatingActionButton(
-                      child:const Icon(Icons.plus_one_outlined),
-                      onPressed: () => {print("Helo world")}
+                    ElevatedButton(
+                      onPressed: _addText,
+                      child:const Icon(Icons.add_rounded),
                     )
                 ],),
-                getTextWidgets(text)
+                getTextWidgets(_text)
               ]),
           );
         },
