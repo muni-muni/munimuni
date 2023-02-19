5a87fab360d8980cefc0e0eda117d76a976e48cd
KyahWill <knownbutnotloved@gmail.com>
2023-02-19 20:06:20 +0800

    created mobile app for this

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
+
 import 'package:flutter/material.dart';
-import 'dart:math';
 //Goal for today:
 /// 1. Create a todo-list
 /// 2. Save data in browser/local cache
@@ -13,18 +13,55 @@ class MuniMuni extends StatefulWidget {
 }
 
 class _MuniMuniState extends State<MuniMuni> {
+  List<String> text = ["test1", "test2", "test3"];
+  
+  Widget getTextWidgets(List<String> strings)
+  {
+    return Column(children: strings.map((item) => Text(item)).toList());
+  }  
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-        title: const Text('Home')
-        )
-      ),
+      routes: <String, WidgetBuilder> {
+        '/': (BuildContext context) {
+          return Scaffold(
+            appBar: AppBar(
+              title: const Text('Home')
+            ),
+            body: Flex(
+              direction: Axis.vertical,
+              children:[
+                Row(
+                  mainAxisSize: MainAxisSize.max,
+                  children: [
+                    const SizedBox(
+                      width:100,
+                      child: TextField(maxLength: 20,)
+                    ),
+                    FloatingActionButton(
+                      child:const Icon(Icons.plus_one_outlined),
+                      onPressed: () => {print("Helo world")}
+                    )
+                ],),
+                getTextWidgets(text)
+              ]),
+          );
+        },
+        '/projects': (BuildContext context ){
+          return Scaffold(
+            appBar: AppBar(
+              title:const Text("test home")
+            ),
+            
+          );
+        }
+      }
+
     );
   }
 }
 void main() {
-  runApp(MuniMuni());
+  runApp(const MuniMuni());
 }
 
